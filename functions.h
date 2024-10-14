ff6ddcad98691dc18a306279a557205ba6743ac7
Maddie <141688377+ANTPOTTER@users.noreply.github.com>
2024-10-15 09:22:57 +1000

    Update functions.h
    added some functions

--- functions.h
+++ functions.h
@@ -10,6 +10,10 @@
 #include <algorithm>
 #include <vector>
 
+// Commands
+std::vector<std::string> code_words(std::vector<std::string> program_vector, int program_count, std::vector<std::string> commands_vector, int commands_count, std::vector<std::string> commandType);
+std::string command_creation(int command_type, std::string content, int program_counter);
+
 // File Reading
 std::string ReadFile(const std::string& FileName);
 void WriteToFile(const std::string& FileName, const std::string& Content);
@@ -18,5 +22,6 @@ void WriteToFile(const std::string& FileName, const std::string& Content);
 std::string replace(std::string str, int oldSubstr, int newSubstr);
 int command_count();
 std::string processVectorUntilChar(std::vector<std::string> vec, int startIndex, std::string targetChar);
+std::vector<std::string> ProcessStringUntilClose(const std::vector<std::string>& code, int startIndex, std::string close_char = "}");
 
 #endif
