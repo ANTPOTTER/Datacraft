157b8d68be8146d686627e8fea9b751e6d55ac70
Maddie <141688377+ANTPOTTER@users.noreply.github.com>
2024-10-16 08:09:22 +1000

    Update functions.h
    added function for finding a word in a vector easily

--- functions.h
+++ functions.h
@@ -24,5 +24,6 @@ int command_count();
 std::string processVectorUntilChar(std::vector<std::string> vec, int startIndex, std::string targetChar);
 std::vector<std::string> ProcessStringUntilClose(const std::vector<std::string>& code, int startIndex, std::string close_char = "}");
 bool containsAnyCharacter(const std::string& str, const char arr[], size_t arrSize);
+bool vectorcontainsword(std::string str, std::vector<std::string> vector, int skip_count = 1);
 
 #endif
