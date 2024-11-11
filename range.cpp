012c96bdf904635ffe213ff2e1abb5a739d44a39
Maddie <141688377+ANTPOTTER@users.noreply.github.com>
2024-11-11 10:06:50 +1000

    Create range.cpp
    made base structure for range_loop function

---
+++ range.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>  
+#include <filesystem>
+#include <algorithm>
+#include <vector>
+
+std::vector<std::string> range_loop(std::vector<std::string> program_vector, int program_count) {
+  return;
+}
