915dc2d073663b71e9239abeb92db2984521c783
Maddie <141688377+ANTPOTTER@users.noreply.github.com>
2024-11-25 14:10:12 +1000

    Update main.cpp
    half way through adding file support

--- main.cpp
+++ main.cpp
@@ -52,6 +52,13 @@ int main(int argc, char* argv[]) {
     int program_count = 0;
     code = code_words(code_vector, 0, commands_vector, 0, command_type);
     
+    std::vector<std::string> result;
+    std::stringstream ss(str);
+    std::string token;
+    while (std::getline(ss, token, delimiter)) {
+        result.push_back(token);
+    }
+
     while (control <= file) {
         
     }
